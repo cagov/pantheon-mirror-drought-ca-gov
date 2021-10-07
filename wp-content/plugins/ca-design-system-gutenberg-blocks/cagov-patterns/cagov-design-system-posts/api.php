79d2e4c0cddd5f35c79b9ed4eb8fccc4511efb4f
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-10-06 20:43:39 -0700

    some notes on date sorting, not working with metabox field

--- wp-content/plugins/ca-design-system-gutenberg-blocks/cagov-patterns/cagov-design-system-posts/api.php
+++ wp-content/plugins/ca-design-system-gutenberg-blocks/cagov-patterns/cagov-design-system-posts/api.php
@@ -13,18 +13,28 @@ function cagov_design_system_posts_detail__init()
     // Add post detail metadata to WP-API
     add_action('rest_api_init', 'cagov_design_system_posts_register_custom_rest_fields');
     
-    // Adjust excerpt behavior to return intended excerpts
-    // add_filter('get_the_excerpt', 'cagov_design_system_posts_detail_excerpt');
-    
     add_filter( 'rest_post_collection_params', 'cagov_design_system_filter_posts_add_rest_orderby_params', 10, 2 );
+
+    // add_filter( 'rest_post_query', 'cagov_design_system_filter_posts_add_rest_post_query', 10, 2);
+
 }
 
 function cagov_design_system_posts_register_custom_rest_fields() {
     register_rest_field(
         'post',
-        'post',
+        'custom_post_date',
         array(
-            'get_callback'    => 'cagov_design_system_posts_get_custom_fields',
+            'get_callback'    => 'cagov_design_system_rest_custom_post_date',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+
+    register_rest_field(
+        'post',
+        'meta',
+        array(
+            'get_callback'    => 'cagov_design_system_get_post_custom_fields',
             'update_callback' => null,
             'schema'          => null,
         )
@@ -40,7 +50,7 @@ function cagov_design_system_posts_get_custom_fields()
 
 function cagov_design_system_get_post_custom_fields($post)
 {
-    $blocks = parse_blocks($post->post_content);
+    global $post;
     try {
         $custom_post_link = get_post_meta($post->ID, '_ca_custom_post_link', true);
         $custom_post_date = get_post_meta($post->ID, '_ca_custom_post_date', true);
@@ -58,9 +68,46 @@ function cagov_design_system_get_post_custom_fields($post)
     return null;
 }
 
+function cagov_design_system_rest_custom_post_date($post)
+{
+    global $post;
+    try {
+        $custom_post_date = get_post_meta($post->ID, '_ca_custom_post_date', true);
+        return $custom_post_date;
+        
+    } catch (Exception $e) {
+    } finally {
+    }
+    return null;
+}
+
 function cagov_design_system_filter_posts_add_rest_orderby_params ( $params ) {
     if (isset($params) && is_array($params)) {
-	    $params["orderby"] = array("_ca_custom_post_date" => "desc");
+	    $params["orderby"]['enum'] = "custom_post_date";
     }
 	return $params;
 }
+
+// function cagov_design_system_filter_posts_add_rest_post_query($query_vars, $request) {
+//     $orderby = $request->get_param('orderby');
+    
+//     if (isset($orderby) && $orderby === 'custom_post_date') {
+//         // $query_vars["orderBy"] = "custom_post_date";
+//         // $query_vars["meta_key"] = "custom_post_date";
+//         // $query_vars["order"] = "desc";
+
+
+//         $query_vars = array(
+//             'post_type' => 'post',
+//             'order' => 'DESC',
+//             'orderby'   => 'custom_post_date',   
+//             // 'meta_query' => array(
+//             //     'custom_post_date' => array(
+//             //         'key' => 'post'
+//             //     )
+//             // )
+//         );
+//     }
+//     return $query_vars;
+// }
+
